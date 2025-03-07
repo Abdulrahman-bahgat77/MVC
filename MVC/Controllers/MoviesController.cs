61eafe4953068853748eb91eb2ef9ed63247c22e
abdulrahman <abdulrahman403868@fci.bu.edu.eg>
2025-03-08 01:24:31 +0200

    Add project files.

---
+++ MVC/Controllers/MoviesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public void GetMovie()
+        {
+
+        }
+       public string Index()
+        {
+            return "Hello Index";
+        }
+    }
+}
